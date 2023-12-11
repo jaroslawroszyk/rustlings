4ade7aa0e7227c831b629545f5b6ac34fbac6114
Jaroslaw <roszyk.jarek@gmail.com>
2023-12-11 01:44:35 +0100

    some exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+
+fn calculate_price_of_apples(apples: i32) -> i32
+{
+    let cost = if apples > 40 { 1 } else { 2 };
+    cost * apples
+}
+
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
